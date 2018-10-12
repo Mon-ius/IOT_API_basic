53b94de6703e7702d5655480253c94d4aa96e06c
monius <m1105930979@gmail.com>
2018-10-13 07:24:11 +0800

    add extra method

--- deploy/init.py
+++ deploy/init.py
@@ -1,13 +1,20 @@
 import getpass
 import os
 import re
+import shutil
 
 ## Configuration file
 uWSGI = "uWSGI.INI"
 NGINX = "NGINX.CONF"
+
 SSL = "SSL_AUTO.SH"
-### Need to change
+SERVICE = "SAMPLE.service"
+DOCKER = "DOCKER.run"
+### May need to change
 DOMAIN = "fff.com"
+NGINX_CONF = "/etc/nginx/sites-enabled/"
+SYSTEMD_CONF = "/etc/systemd/system/"
+
 
 
 def get_env():
@@ -15,7 +22,15 @@ def get_env():
     cwd = os.getcwd()
     return cu,cwd
 
-def gen_ini(usr,loc):
+def add_env(**args):
+    env = os.path.dirname(loc)
+    with open(env+'.env', 'a') as file:
+        for i,j in args.items():
+            file.write(i+'='+j)
+        file.close()
+    return False
+
+def add_uwsgi(usr,loc):
     env = os.path.dirname(loc)
    
     with open(uWSGI, 'r') as fh:
@@ -31,7 +46,7 @@ def gen_ini(usr,loc):
     
         fh.close()
 
-def gen_nginx(loc):
+def add_nginx(loc):
     with open(NGINX, "r") as fh:
         fds = fh.read()
     
@@ -41,17 +56,63 @@ def gen_nginx(loc):
         with open(DOMAIN+'.conf', 'w') as confnginx:
             confnginx.write(res)
             confnginx.close()
-    
+            try:
+                shutil.move(DOMAIN+'.conf',NGINX_CONF)
+                os.system("nginx -s reload")
+            except shutil.Error:
+                print("Complete")
         fh.close()
 
-def accept_warning(s):
-    c = ''
-    d = {'Y': True, 'y': True, 'N': False, 'n': False}
-    while not c in d:
-        c = input('Warning: %s Y/N? ' % s)
-    return d[c]
+def add_service(usr,loc):
+    with open(SERVICE, "r") as fh:
+        fds = fh.read()
+    
+        fcd = re.sub(r'{{domain}}', DOMAIN, fds)
+        fcu = re.sub(r'{{usr}}', usr, fcd)
+        res = re.sub(r'{{loc}}', loc, fcu)
 
-def ssl_init():
+        with open(DOMAIN+'.service', 'w') as confservice:
+            confservice.write(res)
+            confservice.close()
+    
+        #cp
+            try:
+                shutil.move(DOMAIN+'.service',SYSTEMD_CONF)
+            except shutil.Error:
+                print("Complete")
+        
+        os.system("systemctl enable"+DOMAIN)
+        os.system("systemctl start"+DOMAIN)
+        #reload deamon
+        fh.close()
+
+def init_docker(usr):
+    
+    with open(DOCKER, "r") as fh:
+        fds = fh.read()
+    
+        fcd = re.sub(r'{{domain}}', DOMAIN, fds)
+        fcu = re.sub(r'{{usr}}', usr, fcd)
+        res = re.sub(r'{{passwd}}', loc+DOMAIN+usr, fcu)
+
+        with open(DOMAIN+'.run', 'w') as confdocker:
+            confdocker.write(res)
+            confdocker.close()
+    
+        fh.close()
+        
+        os.chmod(DOMAIN+'.run',0o700)
+        os.system(DOMAIN+'.run')
+
+        conf = "postgres://{}:{}@172.17.0.1:5432/{}".format(usr,loc+DOMAIN+usr,DOMAIN)
+        add_env(DATABASE_URL=conf)
+        # os.remove(DOMAIN+'.run')
+
+        #add DATABASE_URL into .env
+
+
+
+def init_ssl():
     key = input("Cloudflare API token #Ex: dah2dsadscangh : ")
     email = input("Cloudflare email #Ex: sexybuddy@fff.com : ")
 
@@ -68,17 +129,31 @@ def ssl_init():
             confssl.close()
 
         fh.close()
+        if not os.path.exists("/etc/nginx/certs"):
+            os.makedirs("/etc/nginx/certs")
         os.chmod(DOMAIN+'.sh',0o700)
         os.system(DOMAIN+'.sh')
-        os.remove(DOMAIN+'.sh')
+        # os.remove(DOMAIN+'.sh')
+
+def accept_warning(s):
+    c = ''
+    d = {'Y': True, 'y': True, 'N': False, 'n': False}
+    while not c in d:
+        c = input('Warning: %s Y/N? ' % s)
+    return d[c]
 
 if __name__ == '__main__':
+    usr,loc = get_env()
     domain = input("Input domain name #Ex: superservice.fff.com :")
     if domain and len(domain)>3:
         DOMAIN = domain
     if not accept_warning("Do you have ssl cert"):
-        ssl_init()
+        init_ssl()
 
-    usr,loc = get_env()
-    gen_ini(usr,loc)
-    gen_nginx(loc)
+    if not accept_warning("Do you have database installed"):
+        init_docker(usr)
+
+    
+    add_uwsgi(usr,loc)
+    add_nginx(loc)
+    add_service(usr,loc)
