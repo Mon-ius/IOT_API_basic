2514c5fda893423809b469f2c7760724572a3997
monius <m1105930979@gmail.com>
2018-11-25 11:58:42 +0800

    add

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app
-from app.models import User,Data,Sensor
+from app.models import User,Data,Sensor,Temperature
 from ext import db,desc
 import click
 app = create_app()
@@ -8,10 +8,11 @@ def clearDB():
     Data.query.delete()
     Sensor.query.delete()
     User.query.delete()
+    Temperature.query.delete()
     db.session.commit()
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Sensor': Sensor, 'Data': Data,'cl':clearDB}
+    return {'db': db, 'User': User, 'Sensor': Sensor, 'Data': Data,'Temp':Temperature,'cl':clearDB}
 
 if __name__ == '__main__': 
     app.run(debug=True)  
